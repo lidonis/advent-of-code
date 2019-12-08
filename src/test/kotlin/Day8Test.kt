8c56ed47b1fdc27ab087c2a73d40db74854855e8
Lidonis Calhau <lidonis.calhau@gmail.com>
2019-12-08 12:28:53 +0100

    refactor day 8

--- src/test/kotlin/Day8Test.kt
+++ src/test/kotlin/Day8Test.kt
@@ -2,8 +2,14 @@ import org.junit.Assert
 import org.junit.Test
 
 internal class Day8Test {
+
     @Test
-    fun getImage() {
+    fun checksum() {
+        val decoder = ImageDecoder("0222112222120000", 2, 2)
+        Assert.assertEquals(4, decoder.checksum())    }
+
+    @Test
+    fun `decode image`() {
         val decoder = ImageDecoder("0222112222120000", 2, 2)
         Assert.assertEquals(listOf("01","10"), decoder.decode())
     }
