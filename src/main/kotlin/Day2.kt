26c326d43e8c4172fa1a1185ad91825a7b87f607
lcalhau <lidonis.calhau@lyra-network.com>
2019-12-13 09:53:56 +0100

    day 13

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -2,7 +2,7 @@ fun main() {
     val computer = IntCodeComputer(
         InputReader("day2.txt").asLinesOfLongs()[0]
     )
-    computer.input(12, 2)
+    computer.input2(12, 2)
     println(computer.asSequence().last()[0])
     computer.reset()
     val inputs = findInputs(computer)
@@ -12,7 +12,7 @@ fun main() {
 fun findInputs(computer: IntCodeComputer): Pair<Long, Long> {
     for (noun in 0..99L) {
         for (verb in 0..99L) {
-            computer.input(noun, verb)
+            computer.input2(noun, verb)
             if (computer.asSequence().last()[0] == 19690720L) {
                 return Pair(noun, verb)
             }
