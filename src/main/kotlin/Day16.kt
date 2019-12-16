14ce9c535a4d318a45068d923907f3e2063135a2
Lidonis Calhau <lidonis.calhau@gmail.com>
2019-12-16 20:52:10 +0100

    finish day 16

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,22 +1,24 @@
 import kotlin.math.abs
 
 fun main() {
-    val input = InputReader("day16.txt").asLineOfLong()
-    println(FlawedFrequencyTransmission.phases(input, 100).take(8).joinToString(""))
+    val times = 100
+    val input = InputReader("day16.txt")
+    println(FlawedFrequencyTransmission.embedded(input.asLineOfInt(), times))
 }
+
 
 object FlawedFrequencyTransmission {
     private val basePattern = listOf(1, 0, -1, 0)
 
-    fun phases(input: List<Long>, times: Int): List<Long> {
+    fun phases(input: List<Int>, times: Int): String {
         var output = input
         repeat(times) {
             output = phase(output)
         }
-        return output
+        return output.take(8).joinToString("")
     }
 
-    fun phase(input: List<Long>) =
+    fun phase(input: List<Int>) =
         input.mapIndexed { i, _ ->
             abs(input.drop(i).zip(repeatingPattern(i).take(input.size).toList()).map {
                 it.first * it.second
@@ -25,12 +27,20 @@ object FlawedFrequencyTransmission {
 
     private fun repeatingPattern(i: Int) = sequence {
         while (true) {
-            yieldAll(pattern(i))
+            yieldAll(basePattern.map { generateSequence { it }.take(i + 1).toList() }
+                .flatten())
         }
     }
 
-    private fun pattern(i: Int) =
-        basePattern.map { generateSequence { it }.take(i + 1).toList() }
-            .flatten()
+    fun embedded(input: List<Int>, times: Int): String {
+        val offset = input.take(7).fold(0) { acc, digit -> 10 * acc + digit }
+        val value = (offset until 10000 * input.size).map { input[it % input.size] }.toIntArray()
+        repeat(times) {
+            value.indices.reversed().fold(0) { acc, i ->
+                (abs(acc + value[i]) % 10).also { value[i] = it }
+            }
+        }
+        return value.take(8).joinToString("")
+    }
 
 }
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -6,31 +6,31 @@ internal class Day16Test {
     @Test
     fun `12345678 phase is 48226158`() {
         val phased = FlawedFrequencyTransmission.phase(listOf(1, 2, 3, 4, 5, 6, 7, 8))
-        Assert.assertEquals(listOf(4L, 8, 2, 2, 6, 1, 5, 8), phased)
+        Assert.assertEquals(listOf(4, 8, 2, 2, 6, 1, 5, 8), phased)
     }
 
     @Test
     fun `48226158 phase is 34040438`() {
         val phased = FlawedFrequencyTransmission.phase(listOf(4, 8, 2, 2, 6, 1, 5, 8))
-        Assert.assertEquals(listOf(3L, 4, 0, 4, 0, 4, 3, 8), phased)
+        Assert.assertEquals(listOf(3, 4, 0, 4, 0, 4, 3, 8), phased)
     }
 
     @Test
     fun `34040438 phase is 03415518`() {
         val phased = FlawedFrequencyTransmission.phase(listOf(3, 4, 0, 4, 0, 4, 3, 8))
-        Assert.assertEquals(listOf(0L, 3, 4, 1, 5, 5, 1, 8), phased)
+        Assert.assertEquals(listOf(0, 3, 4, 1, 5, 5, 1, 8), phased)
     }
 
     @Test
     fun `03415518 phase is 01029498`() {
         val phased = FlawedFrequencyTransmission.phase(listOf(0, 3, 4, 1, 5, 5, 1, 8))
-        Assert.assertEquals(listOf(0L, 1, 0, 2, 9, 4, 9, 8), phased)
+        Assert.assertEquals(listOf(0, 1, 0, 2, 9, 4, 9, 8), phased)
     }
 
     @Test
     fun `12345678 phased 4 times is 01029498`() {
         val phased = FlawedFrequencyTransmission.phases(listOf(1, 2, 3, 4, 5, 6, 7, 8), 4)
-        Assert.assertEquals(listOf(0L, 1, 0, 2, 9, 4, 9, 8), phased)
+        Assert.assertEquals("01029498", phased)
     }
 
     @Test
@@ -38,7 +38,7 @@ internal class Day16Test {
         val phased = FlawedFrequencyTransmission.phases(
             listOf(8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0, 8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5), 100
         )
-        Assert.assertEquals(listOf(2L, 4, 1, 7, 6, 1, 7, 6), phased.take(8))
+        Assert.assertEquals("24176176", phased.take(8))
     }
 
     @Test
@@ -46,7 +46,7 @@ internal class Day16Test {
         val phased = FlawedFrequencyTransmission.phases(
             listOf(1, 9, 6, 1, 7, 8, 0, 4, 2, 0, 7, 2, 0, 2, 2, 0, 9, 1, 4, 4, 9, 1, 6, 0, 4, 4, 1, 8, 9, 9, 1, 7), 100
         )
-        Assert.assertEquals(listOf(7L, 3, 7, 4, 5, 4, 1, 8), phased.take(8))
+        Assert.assertEquals("73745418", phased)
     }
 
     @Test
@@ -54,6 +54,30 @@ internal class Day16Test {
         val phased = FlawedFrequencyTransmission.phases(
             listOf(6, 9, 3, 1, 7, 1, 6, 3, 4, 9, 2, 9, 4, 8, 6, 0, 6, 3, 3, 5, 9, 9, 5, 9, 2, 4, 3, 1, 9, 8, 7, 3), 100
         )
-        Assert.assertEquals(listOf(5L, 2, 4, 3, 2, 1, 3, 3), phased.take(8))
+        Assert.assertEquals("52432133", phased)
+    }
+
+    @Test
+    fun `84462026 is embedded in 03036732577212944063491565474664`() {
+        val embedded = FlawedFrequencyTransmission.embedded(
+            listOf(0,3,0,3,6,7,3,2,5,7,7,2,1,2,9,4,4,0,6,3,4,9,1,5,6,5,4,7,4,6,6,4), 100
+        )
+        Assert.assertEquals("84462026", embedded)
+    }
+
+    @Test
+    fun `78725270 is embedded in 02935109699940807407585447034323`() {
+        val embedded = FlawedFrequencyTransmission.embedded(
+            listOf(0,2,9,3,5,1,0,9,6,9,9,9,4,0,8,0,7,4,0,7,5,8,5,4,4,7,0,3,4,3,2,3), 100
+        )
+        Assert.assertEquals("78725270", embedded)
+    }
+
+    @Test
+    fun `53553731 is embedded in 03081770884921959731165446850517`() {
+        val embedded = FlawedFrequencyTransmission.embedded(
+            listOf(0,3,0,8,1,7,7,0,8,8,4,9,2,1,9,5,9,7,3,1,1,6,5,4,4,6,8,5,0,5,1,7), 100
+        )
+        Assert.assertEquals("53553731", embedded)
     }
 }
