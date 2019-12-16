c16d6287af9c40d6768e47c5143e2a1f0e90c2dc
Lidonis Calhau <lidonis.calhau@gmail.com>
2019-12-16 20:31:58 +0100

    fix input reader

--- src/main/kotlin/InputReader.kt
+++ src/main/kotlin/InputReader.kt
@@ -11,7 +11,7 @@ class InputReader(fileName: String) {
     fun asLinesOfInt() = lines.map { it.toInt() }
     fun asLinesOfLongs() = lines.map { line -> line.split(",").map { it.toLong() } }
     fun asLinesOfStrings() = lines.map { line -> line.split(",") }
-    fun asLineOfLong() = lines[0].chars().mapToLong() { it.toLong() }.toList()
+    fun asLineOfInt() = lines[0].map { Character.digit(it, 10) }
     fun asIntRange(): IntRange {
         val values = lines[0].split("-").map { it.toInt() }
         return values[0]..values[1]
