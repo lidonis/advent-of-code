02ed15e5793b427562cac378e88e6806661b543c
lcalhau <lidonis.calhau@lyra-network.com>
2019-12-17 18:13:11 +0100

    day17 part2 in progress

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,25 +1,20 @@
+import java.lang.IllegalStateException
+
 fun main() {
     val input = InputReader("day17.txt").asLineOfLongs()
     val ascii = AftScaffoldingControlAndInformationInterface(input)
-    ascii.draw()
-    println(ascii.scaffoldMap())
+    //println(ascii.sumOfTheAlignmentParameters())
+    ascii.amountOfDustCollected()
 }
 
 class AftScaffoldingControlAndInformationInterface(private val program: List<Long>) {
 
     private val computer = IntCodeComputer(program)
 
-    fun draw() {
-        var cameraValue = computer.nextOutput()
-        while (cameraValue != null) {
-            print(cameraValue.toChar())
-            cameraValue = computer.nextOutput()
-        }
-        computer.reset()
-    }
+    private val scaffolds = mutableListOf<Position>()
+    private var vacuumRobotPosition = Position(0, 0)
 
-    fun scaffoldMap(): Int {
-        val scaffolds = mutableListOf<Position>()
+    init {
         var i = 0
         var j = 0
         var cameraValue = computer.nextOutput()
@@ -30,18 +25,53 @@ class AftScaffoldingControlAndInformationInterface(private val program: List<Lon
                     scaffolds.add(Position(i, j))
                     i++
                 }
+                '^' -> {
+                    vacuumRobotPosition = Position(i, j)
+                    i++
+                }
+                '.' -> {
+                    i++
+                }
                 '\n' -> {
                     j++
                     i = 0
                 }
-                else -> i++
+                else -> throw IllegalStateException("Unknown map character")
             }
             cameraValue = computer.nextOutput()
         }
-        return scaffolds.filter { getNeighboursCount(scaffolds, it) == 4 }.map { (i,j) -> i * j }.sum()
+        computer.reset()
+    }
+
+
+    fun sumOfTheAlignmentParameters(): Int {
+        return scaffolds.filter { getNeighboursCount(scaffolds, it) == 4 }.map { (i, j) -> i * j }.sum()
     }
 
     private fun getNeighboursCount(scaffolds: List<Position>, intersection: Position) =
         Direction.values().map { intersection.move(it) }.count { scaffolds.contains(it) }
+
+    fun amountOfDustCollected(){
+        computer[0] = 2
+        computer.input('A'.toLong())
+        computer.input(','.toLong())
+        computer.input('B'.toLong())
+        computer.input('\n'.toLong())
+        computer.input('R'.toLong())
+        computer.input('1'.toLong())
+        computer.input('\n'.toLong())
+        computer.input('2'.toLong())
+        computer.input('\n'.toLong())
+        computer.input('3'.toLong())
+        computer.input('\n'.toLong())
+        computer.input('y'.toLong())
+        computer.input('\n'.toLong())
+
+        var cameraValue = computer.nextOutput()
+        while (cameraValue != null) {
+            print(cameraValue.toChar())
+            cameraValue = computer.nextOutput()
+        }
+    }
 
 }
