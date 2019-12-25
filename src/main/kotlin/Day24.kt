98067697e74ffd308d149097f287ac8746c9ca97
Lidonis Calhau <lidonis.calhau@gmail.com>
2019-12-25 02:10:39 +0100

    day24 done

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -1,15 +1,163 @@
-class Eris(private val bugs: List<Tile>) {
+import Eris.*
+import Eris.State.*
+
+fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
+    val evolutions = mutableSetOf<Int>()
+    var eris = Eris(InputReader("day24.txt").text)
+    while (evolutions.add(eris.biodiversityRating)) {
+        eris = eris.evolve()
+    }
+    println("Biodiversity rating for the first layout that appears twice ${eris.biodiversityRating}")
+}
+
+fun part2() {
+    val eris = RecursiveEris(listOf(Eris(InputReader("day24.txt").text).toRecursive()))
+    val bugCount = eris.evolve(200).count()
+    println("$bugCount bugs are present after 200 minutes")
+}
+
+open class Eris(val scan: List<Tile>) {
     constructor(input: String) : this(input.lines().mapIndexed { i, line ->
         line.mapIndexed { j, c ->
-            Tile(c == '#', Position(j, i))
+            Tile(Position(j, i), State.valueOf(c) ?: error { "Unknown tile" })
         }
     }.flatten())
 
     val biodiversityRating by lazy {
-        bugs.mapIndexedNotNull { i, tile ->
-            if(tile.bug) 2.pow(i) else null
+        scan.mapIndexedNotNull { i, tile ->
+            if (tile.state == BUG) 2.pow(i) else null
         }.sum()
     }
 
-    data class Tile(val bug: Boolean, val position: Position)
+    fun evolve() = Eris(scan.map { tile ->
+        tile to tile.position.neighbours().mapNotNull { position ->
+            scan.find { it.position == position }
+        }.count { it.state == BUG }
+    }.map {
+        evolve(it)
+    })
+
+    private fun evolve(pair: Pair<Tile, Int>): Tile {
+        if (pair.first.state == ERIS) return pair.first
+        val bug = if (pair.first.state == BUG) {
+            pair.second == 1
+        } else {
+            pair.second == 1 || pair.second == 2
+        }
+        return Tile(pair.first.position, if (bug) BUG else EMPTY)
+    }
+
+    fun recursiveEvolve(aboveEris: Eris, belowEris: Eris) = Eris(scan.map { tile ->
+        tile to tile.position.neighbours().map { position ->
+            var neighbourBugsCount = 0
+            if (position.x < 0) neighbourBugsCount += aboveEris.count(Position(1, 2))
+            if (position.x > 4) neighbourBugsCount += aboveEris.count(Position(3, 2))
+            if (position.y < 0) neighbourBugsCount += aboveEris.count(Position(2, 1))
+            if (position.y > 4) neighbourBugsCount += aboveEris.count(Position(2, 3))
+            if (position.x == 2 && position.y == 2) {
+                when (tile.position) {
+                    Position(2, 1) -> neighbourBugsCount += belowEris.count(Direction.U)
+                    Position(3, 2) -> neighbourBugsCount += belowEris.count(Direction.R)
+                    Position(2, 3) -> neighbourBugsCount += belowEris.count(Direction.D)
+                    Position(1, 2) -> neighbourBugsCount += belowEris.count(Direction.L)
+                }
+            }
+            if (scan.find { it.position == position }?.state == BUG) neighbourBugsCount++
+            neighbourBugsCount
+        }.sum()
+    }.map {
+        evolve(it)
+    })
+
+    fun count() = scan.count { it.state == BUG }
+
+    private fun count(direction: Direction) = when (direction) {
+        Direction.U -> scan.filter { it.position.y == 0 }
+        Direction.R -> scan.filter { it.position.x == 4 }
+        Direction.D -> scan.filter { it.position.y == 4 }
+        Direction.L -> scan.filter { it.position.x == 0 }
+    }.count { it.state == BUG }
+
+    private fun count(position: Position) = if (scan.find { it.position == position }?.state == BUG) 1 else 0
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Eris) return false
+
+        if (biodiversityRating != other.biodiversityRating) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return biodiversityRating.hashCode()
+    }
+
+    override fun toString() = '\n' + (0 until 5).flatMap { i ->
+        (0 until 5).map { j ->
+            scan.find { it.position == Position(j, i) }?.state?.c
+        } + '\n'
+    }.joinToString("")
+
+    object EmptyRecursiveEris : Eris((0 until 5).flatMap { i ->
+        (0 until 5).map { j ->
+            if (i == 2 && j == 2) {
+                Tile(Position(i, j), ERIS)
+            } else {
+                Tile(Position(j, i), EMPTY)
+            }
+        }
+    })
+
+    fun toRecursive() = Eris(scan - Tile(Position(2, 2), EMPTY) + Tile(Position(2, 2), ERIS))
+
+    data class Tile(val position: Position, val state: State = EMPTY)
+    enum class State(val c: Char) {
+        BUG('#'), EMPTY('.'), ERIS('?');
+
+        companion object {
+            fun valueOf(c: Char) = values().find { it.c == c }
+        }
+    }
+}
+
+
+data class RecursiveEris(val erises: List<Eris>) {
+    fun evolve(minutes: Int): RecursiveEris {
+        var rerises = this
+        repeat(minutes) { rerises = rerises.evolve() }
+        return rerises
+    }
+
+    private fun evolve(): RecursiveEris {
+        val evolved =
+            (listOf(EmptyRecursiveEris.recursiveEvolve(EmptyRecursiveEris, erises.first())) +
+                    (listOf(EmptyRecursiveEris) + erises + EmptyRecursiveEris).windowed(3, partialWindows = true).map {
+                        when (it.size) {
+                            3 -> it[1].recursiveEvolve(it[0], it[2])
+                            2 -> it[1].recursiveEvolve(it[0], EmptyRecursiveEris)
+                            else -> EmptyRecursiveEris
+                        }
+                    }).toMutableList()
+        if (evolved.first() == EmptyRecursiveEris) {
+            evolved.removeAt(0)
+        }
+        if (evolved.first() == EmptyRecursiveEris) {
+            evolved.removeAt(0)
+        }
+        if (evolved.last() == EmptyRecursiveEris) {
+            evolved.removeAt(evolved.size - 1)
+        }
+        if (evolved.last() == EmptyRecursiveEris) {
+            evolved.removeAt(evolved.size - 1)
+        }
+        return RecursiveEris(evolved)
+    }
+
+    fun count() = erises.map(Eris::count).sum()
 }
--- src/test/kotlin/Day24Test.kt
+++ src/test/kotlin/Day24Test.kt
@@ -1,5 +1,8 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
 
 class Day24Test {
 
@@ -14,4 +17,90 @@ class Day24Test {
         )
         assertThat(eris.biodiversityRating).isEqualTo(2129920)
     }
+
+    @ParameterizedTest(name = "Evolve after {index} minute")
+    @MethodSource
+    fun evolve(evolution: Pair<Eris, Eris>) {
+        assertThat(evolution.first.evolve()).isEqualTo(evolution.second)
+    }
+
+
+    @Test
+    fun `evolve example recursively`() {
+        val erises = readErises("day24/part2.txt")
+        val recursiveEris = RecursiveEris(erises.subList(0, 1))
+        assertThat(recursiveEris.evolve(10)).isEqualTo(RecursiveEris(erises.drop(1)))
+    }
+
+    @Test
+    internal fun `evolve recursively 1`() {
+        val recursiveEris = RecursiveEris(
+            listOf(
+                Eris(
+                    input = "....#\n" +
+                            "#..#.\n" +
+                            "#.?##\n" +
+                            "..#..\n" +
+                            "#...."
+                )
+            )
+        )
+        assertThat(recursiveEris.evolve(1)).isEqualTo(
+            RecursiveEris(
+                listOf(
+                    Eris(
+                        input = ".....\n" +
+                                "..#..\n" +
+                                "..?#.\n" +
+                                "..#..\n" +
+                                "....."
+                    ),
+                    Eris(
+                        input = "#..#.\n" +
+                                "####.\n" +
+                                "##?.#\n" +
+                                "##.##\n" +
+                                ".##.."
+                    ),
+                    Eris(
+                        input = "....#\n" +
+                                "....#\n" +
+                                "..?.#\n" +
+                                "....#\n" +
+                                "#####"
+                    )
+
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `count bugs`() {
+        val erises = readErises("day24/part2.txt")
+        assertThat(RecursiveEris(erises.drop(1)).count()).isEqualTo(99)
+    }
+
+    @Test
+    fun `evolve recursive and count bugs`() {
+        val erises = readErises("day24/part2.txt")
+        assertThat(RecursiveEris(erises.subList(0,1)).evolve(10).count()).isEqualTo(99)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun evolve() =
+            readErises("day24/part1.txt").zipWithNext()
+
+        private fun readErises(fileName: String) =
+            InputReader(fileName).lines
+                .asSequence()
+                .filterNot { it.isEmpty() || it[0].isLetter() }
+                .chunked(5)
+                .map { it.joinToString("\n") }
+                .map(::Eris)
+                .toList()
+    }
+
 }
