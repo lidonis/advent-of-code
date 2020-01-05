4eaf3185f87136361f5238de94a4216d7af9bbd4
Lidonis Calhau <lidonis.calhau@gmail.com>
2020-01-05 20:19:00 +0100

    rename tests

--- src/test/kotlin/fr/lidonis/adventofcode/y2019/day1/SpacecraftTest.kt
+++ src/test/kotlin/fr/lidonis/adventofcode/y2019/day1/SpacecraftTest.kt
@@ -1,49 +1,48 @@
 package fr.lidonis.adventofcode.y2019.day1
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class SpacecraftTest {
 
     @Test
-    fun `fuel requirements for mass of 12`() {
+    fun `fuel requirements for mass of 12 is 2`() {
         val spacecraft = Spacecraft(listOf(12))
         assertThat(spacecraft.fuelRequirements()).isEqualTo(2)
     }
 
     @Test
-    fun `fuel requirements for mass of 14`() {
+    fun `fuel requirements for mass of 14 is 2`() {
         val spacecraft = Spacecraft(listOf(14))
         assertThat(spacecraft.fuelRequirements()).isEqualTo(2)
     }
 
     @Test
-    fun `fuel requirements for mass of 1969`() {
+    fun `fuel requirements for mass of 1969 is 654`() {
         val spacecraft = Spacecraft(listOf(1969))
         assertThat(spacecraft.fuelRequirements()).isEqualTo(654)
     }
 
     @Test
-    fun `fuel requirements for mass of 100756`() {
+    fun `fuel requirements for mass of 100756 is 33583`() {
         val spacecraft = Spacecraft(listOf(100756))
         assertThat(spacecraft.fuelRequirements()).isEqualTo(33583)
     }
 
     @Test
-    fun `fuel requirements for mass of 12 and 14`() {
+    fun `fuel requirements for mass of 12 and 14 is 4`() {
         val spacecraft = Spacecraft(listOf(12, 14))
         assertThat(spacecraft.fuelRequirements()).isEqualTo(4)
     }
 
     @Test
-    fun `full fuel requirements for mass of 1969`() {
+    fun `full fuel requirements for mass of 1969 is 966`() {
         val spacecraft = Spacecraft(listOf(1969))
         assertThat(spacecraft.fullFuelRequirements()).isEqualTo(966)
     }
 
     @Test
-    fun `full fuel requirements for mass of 100756`() {
+    fun `full fuel requirements for mass of 100756 is 50346`() {
         val spacecraft = Spacecraft(listOf(100756))
         assertThat(spacecraft.fullFuelRequirements()).isEqualTo(50346)
     }
