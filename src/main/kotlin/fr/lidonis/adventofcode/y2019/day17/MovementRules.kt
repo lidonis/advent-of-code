05c0d0f316014818af286b3696ba9a912f173bd5
Lidonis Calhau <lidonis.calhau@gmail.com>
2020-01-11 22:28:16 +0100

    day 17

--- src/main/kotlin/fr/lidonis/adventofcode/y2019/day17/MovementRules.kt
+++ src/main/kotlin/fr/lidonis/adventofcode/y2019/day17/MovementRules.kt
@@ -4,13 +4,59 @@ data class MovementRules(val mainRoutine: String, val functionA: String, val fun
 
     companion object {
         fun from(path: List<String>): MovementRules {
-            // TODO https://github.com/ephemient/aoc2019/blob/kt/src/main/kotlin/io/github/ephemient/aoc2019/Day17.kt
+            val compressed = compress(path)
+            compressed ?: error("Can't find compressed rules")
+            println(compressed)
             return MovementRules(
-                "A,C,A,C,B,C,B,A,C,B",
-                "R,4,R,10,R,8,R,4",
-                "R,4,L,12,R,6,L,12",
-                "R,10,R,6,R,4"
+                compressed.main.map { i -> 'A' + i }.joinToString(","),
+                compressed.programs[0].joinToString(","),
+                compressed.programs[1].joinToString(","),
+                compressed.programs[2].joinToString(",")
             )
         }
+
+        private fun compress(path: List<String>): CompressState? {
+            val stack = mutableListOf(CompressState(emptyList(), emptyList(), 0))
+            while (stack.isNotEmpty()) {
+                val state = stack.removeAt(stack.lastIndex)
+                if (state.index >= path.size) {
+                    return state
+                }
+                val (main, programs, index) = state
+                if (main.size < 10) {
+                    for ((i, program) in programs.withIndex()) {
+                        if (path.subList(index, minOf(path.size, index + program.size)) ==
+                            program.subList(0, minOf(program.size, path.size - index))
+                        ) {
+                            stack += CompressState(
+                                main + i,
+                                programs,
+                                index + program.size
+                            )
+                        }
+                    }
+                }
+                if (programs.size < 3) {
+                    for (end in index + 1 until path.size) {
+                        val program = path.subList(index, end)
+                        if (program.sumBy { it.length } + program.size - 1 > 20) {
+                            break
+                        }
+                        stack += CompressState(
+                            main + programs.size,
+                            programs.plusElement(program),
+                            index + program.size
+                        )
+                    }
+                }
+            }
+            return null
+        }
+
+        private data class CompressState(
+            val main: List<Int>,
+            val programs: List<List<String>>,
+            val index: Int
+        )
     }
 }
