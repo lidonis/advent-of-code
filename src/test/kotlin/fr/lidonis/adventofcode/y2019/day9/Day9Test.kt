03dfb3f8a339d20601dcd3fab96f019c25eaf309
lcalhau <lidonis.calhau@lyra-network.com>
2020-01-14 16:49:48 +0100

    import assert

--- src/test/kotlin/fr/lidonis/adventofcode/y2019/day9/Day9Test.kt
+++ src/test/kotlin/fr/lidonis/adventofcode/y2019/day9/Day9Test.kt
@@ -1,16 +1,17 @@
 package fr.lidonis.adventofcode.y2019.day9
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day9Test {
 
     @Test
     fun part1() {
-        org.assertj.core.api.Assertions.assertThat(Day9.part1()).isEqualTo(3601950151)
+        assertThat(Day9.part1()).isEqualTo(3601950151)
     }
 
     @Test
     fun part2() {
-        org.assertj.core.api.Assertions.assertThat(Day9.part2()).isEqualTo(64236)
+        assertThat(Day9.part2()).isEqualTo(64236)
     }
 }
