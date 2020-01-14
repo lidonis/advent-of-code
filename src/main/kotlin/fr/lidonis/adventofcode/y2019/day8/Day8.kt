e1647d262c4fb0ebaaa79886e74cf34c3cbf8a4a
lcalhau <lidonis.calhau@lyra-network.com>
2020-01-14 17:43:50 +0100

    fix input day 8

--- src/main/kotlin/fr/lidonis/adventofcode/y2019/day8/Day8.kt
+++ src/main/kotlin/fr/lidonis/adventofcode/y2019/day8/Day8.kt
@@ -6,7 +6,7 @@ private const val IMAGE_WIDTH = 25
 private const val IMAGE_HEIGHT = 6
 
 fun main() {
-    val inputs = InputReader("day8.txt").lines().map { it.split(",") }
+    val inputs = InputReader("input/y2019/day8.txt").lines().map { it.split(",") }
     val decoder = ImageDecoder(inputs[0][0], IMAGE_WIDTH, IMAGE_HEIGHT)
     println(decoder.checksum())
     decoder.decode().map {
