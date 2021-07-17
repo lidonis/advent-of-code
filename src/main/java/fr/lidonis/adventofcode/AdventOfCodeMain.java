290253009b27a4405e624acd04a5e6382abedf8c
lidonis <lidonis.calhau@gmail.com>
2021-07-17 19:10:22 +0200

    log in cli

--- src/main/java/fr/lidonis/adventofcode/AdventOfCodeMain.java
+++ src/main/java/fr/lidonis/adventofcode/AdventOfCodeMain.java
@@ -1,7 +1,9 @@
 package fr.lidonis.adventofcode;
 
+import java.util.logging.Logger;
 import picocli.CommandLine;
 import picocli.jansi.graalvm.AnsiConsole;
+import java.util.logging.Level;
 
 import java.util.concurrent.Callable;
 
@@ -9,6 +11,8 @@ import java.util.concurrent.Callable;
         name = "advent-of-code", mixinStandardHelpOptions = true, description = "Prints Advent of Code result"
 )
 public class AdventOfCodeMain implements Callable<Integer> {
+
+    private static final Logger LOG = Logger.getLogger(AdventOfCodeMain.class.getName());
 
     @CommandLine.Option(names = {"-y", "--year"}, description = "Advent of Code year", required = true)
     private int year;
@@ -29,7 +33,7 @@ public class AdventOfCodeMain implements Callable<Integer> {
         try {
             new AdventOfCodeCommand(year, day).call();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            LOG.log(Level.SEVERE, "Error in the command", e);
             return 1;
         }
         return 0;
