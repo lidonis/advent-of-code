36fa8bee6dd3ff7f004fb26b73c7359602054a03
l.calhau <l.calhau@betclicgroup.com>
2021-12-01 12:19:53 +0100

    fix detekt

--- src/main/kotlin/fr/lidonis/adventofcode/y2021/day1/SweepReport.kt
+++ src/main/kotlin/fr/lidonis/adventofcode/y2021/day1/SweepReport.kt
@@ -1,9 +1,11 @@
 package fr.lidonis.adventofcode.y2021.day1
 
+private const val WINDOW_SIZE = 3
+
 class SweepReport(private val depths: List<Int>) {
     fun countIncrease() = count(depths)
 
-    fun countWindowIncrease() = count(depths.windowed(3).map(List<Int>::sum))
+    fun countWindowIncrease() = count(depths.windowed(WINDOW_SIZE).map(List<Int>::sum))
 
     private fun count(list: List<Int>) = list.zipWithNext().count { it.second > it.first }
 }
