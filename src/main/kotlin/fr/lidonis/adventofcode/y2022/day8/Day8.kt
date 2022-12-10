b66103ab2602647083025d23705010e866b80a1b
lidonis <lidonis.calhau@gmail.com>
2022-12-10 06:52:14 +0100

    day8

---
+++ src/main/kotlin/fr/lidonis/adventofcode/y2022/day8/Day8.kt
@@ -0,0 +1,17 @@
+package fr.lidonis.adventofcode.y2022.day8
+
+import fr.lidonis.adventofcode.common.Answer
+import fr.lidonis.adventofcode.y2022.AdventOfCode2022
+
+private const val DAY = 8
+
+object Day8: AdventOfCode2022(DAY) {
+
+    private val treetopTreeHouse = TreetopTreeHouse(input().lines())
+
+    @Answer("1717")
+    override fun part1() = treetopTreeHouse.visibleTrees()
+
+    @Answer("321975")
+    override fun part2() = treetopTreeHouse.highestScenicScore()
+}
---
+++ src/main/kotlin/fr/lidonis/adventofcode/y2022/day8/TreetopTreeHouse.kt
@@ -0,0 +1,85 @@
+package fr.lidonis.adventofcode.y2022.day8
+
+class TreetopTreeHouse(private val lines: List<String>) {
+    private val width = lines.maxOf { it.length }
+
+    fun visibleTrees(): Int {
+        val visibilities = lines.map { line ->
+            BooleanArray(line.length).apply {
+                var index = 0
+                for (value in line.iterator().scanVisibility()) {
+                    if (value) set(index, true)
+                    index++
+                }
+                for (value in line.reverseIterator().scanVisibility()) {
+                    --index
+                    if (value) set(index, true)
+                }
+            }
+        }
+        repeat(width) {
+            var index = 0
+            for (value in lines.columnIterator(it).scanVisibility()) {
+                if (value) visibilities[index][it] = true
+                index++
+            }
+            for (value in lines.asReversed().columnIterator(it).scanVisibility()) {
+                --index
+                if (value) visibilities[index][it] = true
+            }
+        }
+        return visibilities.sumOf { it.count { it } }
+    }
+
+    fun highestScenicScore(): Int {
+        val scores = lines.map { line ->
+            IntArray(line.length).apply {
+                var index = 0
+                for (value in line.iterator().scanScore()) this[index++] = value
+                for (value in line.reverseIterator().scanScore()) this[--index] *= value
+            }
+        }
+        repeat(width) {
+            var index = 0
+            for (value in lines.columnIterator(it).scanScore()) scores[index++][it] *= value
+            for (value in lines.asReversed().columnIterator(it).scanScore()) scores[--index][it] *= value
+        }
+        return scores.maxOf { it.max() }
+    }
+}
+
+private fun CharIterator.scanVisibility(): BooleanIterator = object : BooleanIterator() {
+    private var isFirst = true
+    private var max = Char.MIN_VALUE
+    override fun hasNext(): Boolean = this@scanVisibility.hasNext()
+    override fun nextBoolean(): Boolean {
+        val char = nextChar()
+        val boolean = if (isFirst) true.also { isFirst = false } else char > max
+        if (boolean) max = char
+        return boolean
+    }
+}
+
+private fun CharIterator.scanScore(): IntIterator = object : IntIterator() {
+    private val seen = StringBuilder()
+    private var index = 0
+    override fun hasNext(): Boolean = this@scanScore.hasNext()
+    override fun nextInt(): Int {
+        val char = nextChar()
+        val int = index++ - seen.indexOfLast { it >= char }.coerceAtLeast(0)
+        seen.append(char)
+        return int
+    }
+}
+
+private fun CharSequence.reverseIterator(): CharIterator = object : CharIterator() {
+    private var index = length
+    override fun hasNext(): Boolean = index > 0
+    override fun nextChar(): Char = get(--index)
+}
+
+private fun List<CharSequence>.columnIterator(column: Int): CharIterator = object : CharIterator() {
+    private var index = 0
+    override fun hasNext(): Boolean = index < size
+    override fun nextChar(): Char = get(index++)[column]
+}
