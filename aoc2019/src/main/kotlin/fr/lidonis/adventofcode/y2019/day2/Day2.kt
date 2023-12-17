941ac3d670da0fd317adea0bf19b02124edd644d
lidonis <lidonis.calhau@gmail.com>
2023-12-17 18:53:47 +0100

    fix //

--- aoc2019/src/main/kotlin/fr/lidonis/adventofcode/y2019/day2/Day2.kt
+++ aoc2019/src/main/kotlin/fr/lidonis/adventofcode/y2019/day2/Day2.kt
@@ -2,6 +2,7 @@ package fr.lidonis.adventofcode.y2019.day2
 
 import fr.lidonis.adventofcode.common.Answer
 import fr.lidonis.adventofcode.y2019.AdventOfCode2019
+import fr.lidonis.adventofcode.y2019.intcodecomputer.IntCodeComputer
 import fr.lidonis.adventofcode.y2019.intcodecomputer.IntCodeComputerFactory
 
 private const val DAY = 2
@@ -15,27 +16,27 @@ object Day2 : AdventOfCode2019(DAY) {
     private const val SEARCHED_OUTPUT = 19690720L
     private const val MULTIPLIER = 100
 
-    private val computer = IntCodeComputerFactory.buildBasicComputer(input().text())
+    private val input = input().text()
 
     @Answer("4138658")
-    override fun part1() = runNounVerb(NOUN, VERB)
+    override fun part1() = IntCodeComputerFactory.buildBasicComputer(input).runNounVerb(NOUN, VERB)
 
     @Answer("7264")
     override fun part2(): Long {
+        val computer = IntCodeComputerFactory.buildBasicComputer(input)
         for (noun in 0..MAX_INPUT) {
             for (verb in 0..MAX_INPUT) {
-                if (runNounVerb(noun, verb) == SEARCHED_OUTPUT) return MULTIPLIER * noun + verb
+                if (computer.runNounVerb(noun, verb) == SEARCHED_OUTPUT) return MULTIPLIER * noun + verb
             }
         }
         error("No inputs found")
     }
 
-    private fun runNounVerb(noun: Long, verb: Long) =
-        computer.run {
-            reset()
-            memory[1] = noun
-            memory[2] = verb
-            run()
-            memory[0]
-        }
+    private fun IntCodeComputer.runNounVerb(noun: Long, verb: Long) = run {
+        reset()
+        memory[1] = noun
+        memory[2] = verb
+        run()
+        memory[0]
+    }
 }
