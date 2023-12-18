e21d211e39fd3abdc8401e02f07ef4615f70519f
lidonis <lidonis.calhau@gmail.com>
2023-12-18 06:02:14 +0100

    fix

--- aoc2021/src/test/kotlin/fr/lidonis/adventofcode/y2021/day16/PacketDecoderTest.kt
+++ aoc2021/src/test/kotlin/fr/lidonis/adventofcode/y2021/day16/PacketDecoderTest.kt
@@ -10,14 +10,14 @@ class PacketDecoderTest {
 
     @Test
     fun part1() {
-        val packetDecoder = PacketDecoder(input.lines())
+        val packetDecoder = PacketDecoder(input)
         val result = packetDecoder.part1()
         assertThat(result).isEqualTo(0)
     }
 
     @Test
     fun part2() {
-        val packetDecoder = PacketDecoder(input.lines())
+        val packetDecoder = PacketDecoder(input)
         val result = packetDecoder.part2()
         assertThat(result).isEqualTo(0)
     }
