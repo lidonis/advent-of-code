3ce2d2913a1a0dc0f08ccb1fb13f3c63783afe93
Lidonis Calhau <lidonis.calhau@gmail.com>
2024-12-03 07:53:20 +0100

    2024 3 refactor

--- aoc2024/src/commonMain/kotlin/fr/lidonis/adventofcode/y2024/day3/MullItOver.kt
+++ aoc2024/src/commonMain/kotlin/fr/lidonis/adventofcode/y2024/day3/MullItOver.kt
@@ -1,39 +1,44 @@
 package fr.lidonis.adventofcode.y2024.day3
 
-private val regex = """(mul\((\d+),(\d+)\)|do\(\)|don't\(\))""".toRegex()
+private val INSTRUCTION_REGEX = """(mul\((\d+),(\d+)\)|do\(\)|don't\(\))""".toRegex()
 
 class MullItOver(program: String) {
 
-    val instructions = regex.findAll(program)
-        .map(MatchResult::destructured)
-        .map { (instruction, x, y) ->
-            when {
-                instruction.startsWith("mul") -> Instruction.Mul(x.toInt(), y.toInt())
-                instruction == "do()" -> Instruction.Do
-                instruction == "don't()" -> Instruction.Dont
-                else -> error("Unknown instruction: $instruction")
-            }
-        }
-        .toList()
+    val instructions = parseInstructions(program)
 
-    fun part1() =
-        instructions.sumOf {
-            when (it) {
-                is Instruction.Mul -> it.calc()
-                else -> 0
-            }
-        }
+    fun part1() = instructions.sumOf { calculateSum(it) }
 
-    fun part2() =
-        instructions.fold(0 to true) { acc, instruction ->
-            val (sum, enabled) = acc
-            when (instruction) {
-                is Instruction.Mul if enabled -> sum + instruction.calc() to true
-                is Instruction.Mul -> acc
-                Instruction.Do -> sum to true
-                Instruction.Dont -> sum to false
-            }
-        }.first
+    fun part2() = calculateConditionalSum()
+
+    private fun parseInstructions(program: String) =
+        INSTRUCTION_REGEX.findAll(program)
+            .map(MatchResult::destructured)
+            .map { (instruction, x, y) -> determineInstruction(instruction, x, y) }
+            .toList()
+
+    private fun determineInstruction(instruction: String, x: String, y: String): Instruction {
+        return when {
+            instruction.startsWith("mul") -> Instruction.Mul(x.toInt(), y.toInt())
+            instruction == "do()" -> Instruction.Do
+            instruction == "don't()" -> Instruction.Dont
+            else -> error("Unknown instruction: $instruction")
+        }
+    }
+
+    private fun calculateSum(instruction: Instruction) = when (instruction) {
+        is Instruction.Mul -> instruction.calc()
+        else -> 0
+    }
+
+    private fun calculateConditionalSum() = instructions.fold(0 to true) { acc, instruction ->
+        val (sum, enabled) = acc
+        when (instruction) {
+            is Instruction.Mul if enabled -> sum + instruction.calc() to true
+            is Instruction.Mul -> acc
+            Instruction.Do -> sum to true
+            Instruction.Dont -> sum to false
+        }
+    }.first
 }
 
 sealed interface Instruction {
